8ae179ddb1eaf37707113434bba1835f03402f5b
tungak2003 <dasuabest@gmail.com>
2023-06-04 19:24:50 +0700

    giao diện homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ManageProductComponent } from './pages/admin/manage-product/manage-product.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+
+  {
+    path: 'admin', component: LayoutAdminComponent, children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'product', component: ManageProductComponent },
+
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ManageProductComponent } from './pages/admin/manage-product/manage-product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LayoutAdminComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    ManageProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
