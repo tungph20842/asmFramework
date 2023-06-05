bccfbfc24432023954ed89b218db7ed61f7d7cb6
synnph24852 <synnph24852@fpt.edu.vn>
2023-06-05 13:09:14 +0700

    sua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.compon
 import { NewProductPageComponent } from './admin/new-product-page/new-product-page.component';
 import { ProductListPageComponent } from './admin/product-list-page/product-list-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { UpdateProductPageComponent } from './admin/update-product-page/update-product-page.component';
 
 
 
@@ -18,6 +19,7 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
   declarations: [
     AppComponent,
     HomePageComponent,
+    UpdateProductPageComponent,
     LayoutAdminComponent,
     NewProductPageComponent,
     PageNotFoundComponent,
