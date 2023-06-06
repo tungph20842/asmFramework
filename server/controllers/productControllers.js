0f7701b19c95b28bf0ddb6d5aa7bf0ab18ce3edc
tungak2003 <dasuabest@gmail.com>
2023-06-06 22:21:29 +0700

    Create-UI-Backend- Get a single product

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -12,3 +12,23 @@ exports.getAllProducts = (req, res) => {
       });
     });
 };
+
+// Get a single product
+exports.getProductById = (req, res) => {
+  const { id } = req.params;
+
+  Product.findById(id)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).send({
+          message: `Product with id ${id} not found`,
+        });
+      }
+      res.send(product);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving product.',
+      });
+    });
+};
